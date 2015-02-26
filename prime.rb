e9b4f8c9d85cd949e197f5d264df8b1b85d89db8
axton21 <axton_n@yahoo.com>
2015-02-26 09:30:32 -0500

    todo?

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+require 'pry'
+
+
+  def prime?(number)
+    index = 2
+    while index < number do
+        if number % index == 0
+          return false
+        end
+    index +=1
+    end
+    true
+  end
